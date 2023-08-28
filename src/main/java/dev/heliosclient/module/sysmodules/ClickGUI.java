00038408e41aeaf4278c5d6a03cdec039b664d78
TomPlaneta <90921042+TomPlaneta@users.noreply.github.com>
2023-08-28 11:17:28 +0200

    Added tooltip mode setting

--- src/main/java/dev/heliosclient/module/sysmodules/ClickGUI.java
+++ src/main/java/dev/heliosclient/module/sysmodules/ClickGUI.java
@@ -5,12 +5,17 @@ import dev.heliosclient.module.Category;
 import dev.heliosclient.module.Module_;
 import dev.heliosclient.module.settings.BooleanSetting;
 import dev.heliosclient.module.settings.ColorSetting;
+import dev.heliosclient.module.settings.CycleSetting;
 import dev.heliosclient.module.settings.Setting;
 import dev.heliosclient.system.ColorManager;
 import dev.heliosclient.ui.ModulesListOverlay;
+import dev.heliosclient.ui.clickgui.Tooltip;
 import dev.heliosclient.util.ColorUtils;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.text.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClickGUI extends Module_ {
     BooleanSetting Pause = new BooleanSetting("Pause game", "Pause the game when Click GUI is on.", this,false);
@@ -19,6 +24,7 @@ public class ClickGUI extends Module_ {
     BooleanSetting RainbowPane = new BooleanSetting("Rainbow", "Rainbow effect for category panes.", this,false);
     ColorSetting PaneTextColor = new ColorSetting("Category pane text color", "Color of pane text.", this, ColorManager.INSTANCE.clickGuiPaneText);
     ColorSetting TextColor = new ColorSetting("Text color", "Color of text all through out the client.", this, ColorManager.INSTANCE.defaultTextColor);
+    CycleSetting TooltipMode = new CycleSetting("Tooltip mode", "Mode in what tooltips should be shown.", this, new ArrayList<String>(List.of("Normal", "Fixed", "Vanilla")), 0);
 
     public static boolean pause = false;
 
@@ -27,6 +33,7 @@ public class ClickGUI extends Module_ {
     public ClickGUI() {
         super("ClickGUI", "ClickGui related stuff.",  Category.RENDER);
         settings.add(Pause);
+        settings.add(TooltipMode);
         settings.add(AccentColor);
         settings.add(RainbowAccent);
         settings.add(PaneTextColor);
@@ -34,6 +41,7 @@ public class ClickGUI extends Module_ {
         settings.add(TextColor);
 
         quickSettings.add(Pause);
+        quickSettings.add(TooltipMode);
         quickSettings.add(RainbowPane);
         quickSettings.add(RainbowAccent);
         quickSettings.add(TextColor);
@@ -42,6 +50,8 @@ public class ClickGUI extends Module_ {
 
     @Override
     public void onSettingChange(Setting setting) {
+        Tooltip.tooltip.mode = TooltipMode.value;
+
         ColorManager.INSTANCE.clickGuiSecondaryAlpha = AccentColor.getA();
         ColorManager.INSTANCE.clickGuiSecondary = AccentColor.value;
         ColorManager.INSTANCE.clickGuiSecondaryRainbow = RainbowAccent.value;
@@ -57,6 +67,8 @@ public class ClickGUI extends Module_ {
     @Override
     public void onLoad() {
         super.onLoad();
+        Tooltip.tooltip.mode = TooltipMode.value;
+
         ColorManager.INSTANCE.clickGuiSecondaryAlpha = AccentColor.getA();
         ColorManager.INSTANCE.clickGuiSecondary = AccentColor.value;
         ColorManager.INSTANCE.clickGuiSecondaryRainbow = RainbowAccent.value;
