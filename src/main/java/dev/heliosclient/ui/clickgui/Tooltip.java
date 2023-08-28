9b32cee816aa0fbedc702e85ec60e251b2451831
TomPlaneta <90921042+TomPlaneta@users.noreply.github.com>
2023-08-28 11:18:12 +0200

    Simplified tooltip rendering

--- src/main/java/dev/heliosclient/ui/clickgui/Tooltip.java
+++ src/main/java/dev/heliosclient/ui/clickgui/Tooltip.java
@@ -17,17 +17,27 @@ public class Tooltip {
     public void render(DrawContext drawContext, TextRenderer textRenderer, int mouseX, int mouseY)
     {
         if (!this.tooltipText.isEmpty()) {
+            int windWidth = drawContext.getScaledWindowWidth();
+            int winHeight = drawContext.getScaledWindowHeight();
+            int textWidth = textRenderer.getWidth(this.tooltipText);
             if (this.mode == 0) {
-                drawContext.fill(mouseX + 1, mouseY - 1, mouseX + textRenderer.getWidth(this.tooltipText) + 5, mouseY - 14, 0xAA000000);
-                drawContext.drawText(textRenderer, this.tooltipText, mouseX + 4, mouseY - 11, ColorManager.INSTANCE.defaultTextColor(), true);
+                this.renderTooltip(drawContext, textRenderer, tooltipText, mouseX + 1, mouseY - 1);
             } else if (this.mode == 1) {
-                drawContext.fill(drawContext.getScaledWindowWidth()-textRenderer.getWidth(this.tooltipText)-6, drawContext.getScaledWindowHeight()-13, drawContext.getScaledWindowWidth(), drawContext.getScaledWindowHeight(), 0xAA000000);
-                drawContext.drawText(textRenderer, this.tooltipText, drawContext.getScaledWindowWidth()-textRenderer.getWidth(this.tooltipText)-2, drawContext.getScaledWindowHeight() -10, ColorManager.INSTANCE.defaultTextColor(), true);
+                int x = windWidth-textWidth-4;
+                int y = winHeight-13;
+                this.renderTooltip(drawContext, textRenderer, tooltipText, windWidth-textWidth-4, winHeight);
             } else if (this.mode == 2) {
                 drawContext.drawTooltip(textRenderer, Text.of(this.tooltipText), mouseX, mouseY);
             }
             }
         this.tooltipText = "";
+        }
+
+
+        private void renderTooltip(DrawContext drawContext, TextRenderer textRenderer, String text, int x, int y) {
+            int textWidth = textRenderer.getWidth(text);
+            drawContext.fill(x, y, x + textWidth + 4, y - 13, 0xAA000000);
+            drawContext.drawText(textRenderer, text, x + 2, y - 10, ColorManager.INSTANCE.defaultTextColor(), true);
         }
 
     public void changeText(String str) {
